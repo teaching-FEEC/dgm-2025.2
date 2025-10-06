8bec4c555a078c5a2712ee13a8a5c9ddfdeeac1d
ana_caznok <a231745@dac.unicamp.br>
2025-10-06 13:57:58 -0300

    experiments with shs gan

---
+++ projects/hyperskin/synthesis_shsgan_exp.sh
@@ -0,0 +1,4 @@
+
+
+python src/main.py fit -c configs/data/hsi_dermoscopy_synth_cropped.yaml -c configs/model/shs_3channels.yaml #testing channel noise inputs
+python src/main.py fit -c configs/data/hsi_dermoscopy_synth_cropped.yaml -c configs/model/shs_fft.yaml #testing fft
