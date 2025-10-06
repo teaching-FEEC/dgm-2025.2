4fb1f9629f761056e4af1356b294f2e5a35caa3b
ana_caznok <a231745@dac.unicamp.br>
2025-10-06 13:59:30 -0300

    correct error

--- projects/hyperskin/synthesis_shsgan_exp.sh
+++ projects/hyperskin/synthesis_shsgan_exp.sh
@@ -1,4 +1,4 @@
 
 
-python src/main.py fit -c configs/data/hsi_dermoscopy_synth_cropped.yaml -c configs/model/shs_3channels.yaml #testing channel noise inputs
-python src/main.py fit -c configs/data/hsi_dermoscopy_synth_cropped.yaml -c configs/model/shs_fft.yaml #testing fft
+python src/main.py fit -c configs/data/hsi_dermoscopy_synth_cropped.yaml -c configs/model/shs_gan_3channels.yaml #testing channel noise inputs
+python src/main.py fit -c configs/data/hsi_dermoscopy_synth_cropped.yaml -c configs/model/shs_gan_fft.yaml #testing fft
