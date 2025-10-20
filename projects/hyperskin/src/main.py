9fcc496c23fe6f0ca8942bd30028f60625c4e0de
Aline <alineyomachado>
2025-10-20 16:56:52 -0300

    testing shs experiments

--- projects/hyperskin/src/main.py
+++ projects/hyperskin/src/main.py
@@ -14,3 +14,6 @@ if __name__ == "__main__":
         debugpy.wait_for_client()
 
     cli = CustomLightningCLI(parser_kwargs={"parser_mode": "omegaconf"})
+
+#python main.py fit -c /mnt/datahdd/kris_volume/dgm-2025.2/projects/hyperskin/configs/data/hsi_dermoscopy_synth_cropped.yaml -c /mnt/datahdd/kris_volume/dgm-2025.2/projects/hyperskin/configs/model/shs_gan.yaml
+
