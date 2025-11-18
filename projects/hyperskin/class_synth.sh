d5ffe6c2b516456e605a9b1f2362c4a0cb360651
ana_caznok <a231745@dac.unicamp.br>
2025-11-18 13:16:51 -0300

    reproducing tests

--- projects/hyperskin/class_synth.sh
+++ projects/hyperskin/class_synth.sh
@@ -3,9 +3,9 @@
 
 #python src/main.py fit -c configs/data/hsi_dermoscopy_val-synth.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
 
-#python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train03.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
-#python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train05.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
-#python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train07.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
+python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train03.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
+python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train05.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
+python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train07.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
 
 
 
@@ -13,4 +13,4 @@
 #python src/main.py fit -c configs/data/hsi_dermoscopy_full-train1_imb.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
 #python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train1_comp.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
 #python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train1_bal.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
-python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train1_imb.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
+#python src/main.py fit -c configs/data/hsi_dermoscopy_mixed-train1_imb.yaml -c configs/model/hsi_classifier_densenet201_best.yaml
