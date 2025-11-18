03a6cfcec22fddf1d6c1a2ced4a169721933d30b
ana_caznok <a231745@dac.unicamp.br>
2025-11-18 13:56:25 -0300

    prepping experimentation

---
+++ projects/hyperskin/aug_exp.sh
@@ -0,0 +1,16 @@
+
+
+# no transforms data augmentation, no pretraining, no synth data, no balanced sampling, no focal loss
+python src/main.py fit -c configs/data/hsi_dermoscopy_croppedv2_notaug.yaml -c configs/model/densenet201_fromscratch.yaml
+
+# no transforms data augmentation, no pretraining, no synth data, YES balanced sampling, no focal loss
+python src/main.py fit -c configs/data/hsi_dermoscopy_croppedv2_notaug_batchr.yaml -c configs/model/densenet201_fromscratch.yaml
+
+# no transforms data augmentation, no pretraining, no synth data, no balanced sampling, YES focal loss
+python src/main.py fit -c configs/data/hsi_dermoscopy_croppedv2_notaug.yaml -c configs/model/densenet201_fromscratch_focalloss.yaml 
+
+# no transforms data augmentation, no pretraining, no synth data, YES balanced sampling, YES focal loss
+python src/main.py fit -c configs/data/hsi_dermoscopy_croppedv2_notaug_batchr.yaml -c configs/model/densenet201_fromscratch_focalloss.yaml
+
+# no transforms data augmentation, no pretraining, YES synth data, no balanced sampling, no focal loss
+python src/main.py fit -c configs/data/hsi_dermoscopy_croppedv2_augsynth.yaml -c configs/model/densenet201_fromscratch.yaml
